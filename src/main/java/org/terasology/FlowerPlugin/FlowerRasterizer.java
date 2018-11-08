9224dab149f55e4a91bbf4e540cdc17d82384408
Graviton <graviton048@gmail.com>
2018-11-08 22:08:16 +0530

    some changes :)

--- src/main/java/org/terasology/FlowerPlugin/FlowerRasterizer.java
+++ src/main/java/org/terasology/FlowerPlugin/FlowerRasterizer.java
@@ -9,25 +9,36 @@ import org.terasology.world.block.BlockManager;
 import org.terasology.world.chunks.CoreChunk;
 import org.terasology.world.generation.Facet;
 import org.terasology.world.generation.Region;
-import org.terasology.world.generation.Requires;
 import org.terasology.world.generation.WorldRasterizerPlugin;;
 import org.terasology.world.generation.facets.SurfaceHeightFacet;
 import org.terasology.world.generator.plugin.RegisterPlugin;
+import java.lang.Math;
+
+
+
+
 
 
 @RegisterPlugin
 public class FlowerRasterizer implements WorldRasterizerPlugin{
     private Block iris;
-    private Block lavender;
+    private Block yellowflower;
 
     @Facet(SurfaceHeightFacet.class)
 
     //@Requires(SurfaceHeightFacet)
 
 
+    public static double getRandomNumber(){
+        double x = Math.random() * (4 - 1);
+        return x;
+    }
+
+
     @Override
     public void initialize(){
         iris = CoreRegistry.get(BlockManager.class).getBlock("Core:Iris");
+        yellowflower = CoreRegistry.get(BlockManager.class).getBlock("Core:YellowFlower");
 
     }
 
@@ -37,7 +48,7 @@ public class FlowerRasterizer implements WorldRasterizerPlugin{
         for (Vector3i position : chunkRegion.getRegion()) {
             float surfaceHeight = surfaceHeightFacet.getWorld(position.x, position.z);
             if (position.y < surfaceHeight + 1 && position.y > surfaceHeight) {
-                chunk.setBlock(ChunkMath.calcBlockPos(position), iris);
+                chunk.setBlock(ChunkMath.calcBlockPos(position), yellowflower);
             }
 
         }
