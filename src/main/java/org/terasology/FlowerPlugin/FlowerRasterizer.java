1e59e1cffb8f6754802a18982b534c4b88086993
Graviton <graviton048@gmail.com>
2018-11-18 12:40:10 +0530

    chages

--- src/main/java/org/terasology/FlowerPlugin/FlowerRasterizer.java
+++ src/main/java/org/terasology/FlowerPlugin/FlowerRasterizer.java
@@ -40,38 +40,38 @@ public class FlowerRasterizer implements WorldRasterizerPlugin {
         for (Entry<BaseVector3i, Flower> entry : flowerFacet.getWorldEntries().entrySet()) {
             // there should be a house here
             // create a couple 3d regions to help iterate through the cube shape, inside and out
-            Vector3i centerFlowerPosition = new Vector3i(entry.getKey());
-            int extent = entry.getValue().getExtent();
-            centerFlowerPosition.add(0, extent, 0);
-            BaseVector3i min =entry.getKey();
-            BaseVector3i max =entry.getKey();
-            Region3i walls = Region3i.createFromMinMax(min, max);
+            //Vector3i centerFlowerPosition = new Vector3i(entry.getKey());
+            //int extent = entry.getValue().getExtent();
+            //centerFlowerPosition.add(0, extent, 0);
+            BaseVector3i min = entry.getKey();
+            BaseVector3i max = entry.getKey();
+            Region3i tower = Region3i.createFromMinAndSize(min  , max);
             //Region3i inside = Region3i.createFromCenterExtents(centerFlowerPosition, extent - 1);
 
             // loop through each of the positions in the cube, ignoring the inside
-            for (Vector3i newBlockPosition : walls) {
+            for (Vector3i newBlockPosition : tower) {
                 //Random random = new Random();
                 //SurfaceHeightFacet surfaceHeightFacet = chunkRegion.getFacet(SurfaceHeightFacet.class);
                 //float surfaceHeight = surfaceHeightFacet.getWorld(position.x, position.z);
                 SeaLevelFacet seaLevelFacet = chunkRegion.getFacet(SeaLevelFacet.class);
                 int seaLevel = seaLevelFacet.getSeaLevel();
                 if(newBlockPosition.y > seaLevel - 1) {
-                    if (newBlockPosition.y > 60) {
+                    if (newBlockPosition.y > 120) {
                         if (chunkRegion.getRegion().encompasses(newBlockPosition)) {
                             chunk.setBlock(ChunkMath.calcBlockPos(newBlockPosition), redflower);
                         }
                     }
-                    else if (newBlockPosition.y <= 60 && newBlockPosition.y > 40) {
+                    else if (newBlockPosition.y <= 120 && newBlockPosition.y > 80) {
                         if (chunkRegion.getRegion().encompasses(newBlockPosition)) {
                             chunk.setBlock(ChunkMath.calcBlockPos(newBlockPosition), yellowflower);
                         }
                     }
-                    else if (newBlockPosition.y <= 40 && newBlockPosition.y > 20) {
+                    else if (newBlockPosition.y <= 80 && newBlockPosition.y > 40) {
                         if (chunkRegion.getRegion().encompasses(newBlockPosition)) {
                             chunk.setBlock(ChunkMath.calcBlockPos(newBlockPosition), irisflower);
                         }
                     }
-                    else if (newBlockPosition.y < 20 && newBlockPosition.y > 0) {
+                    else if (newBlockPosition.y <= 40 && newBlockPosition.y > 0) {
                         if (chunkRegion.getRegion().encompasses(newBlockPosition)) {
                             chunk.setBlock(ChunkMath.calcBlockPos(newBlockPosition), lavenderflower);
                         }
